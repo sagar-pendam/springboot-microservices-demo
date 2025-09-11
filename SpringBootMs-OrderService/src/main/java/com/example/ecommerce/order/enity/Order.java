860d4f95e317c119814099c1be2c091c397b6c8b
pendamsagar <pendamsagar3@gmail.com>
2025-09-11 19:39:52 +0530

    initial commit 2

--- SpringBootMs-OrderService/src/main/java/com/example/ecommerce/order/enity/Order.java
+++ SpringBootMs-OrderService/src/main/java/com/example/ecommerce/order/enity/Order.java
@@ -1,10 +1,23 @@
 package com.example.ecommerce.order.enity;
 
-import jakarta.persistence.*;
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "orders")
-public class Order {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- SpringBootMs-OrderService/src/main/java/com/example/ecommerce/order/rest/OrderOperationController.java
+++ SpringBootMs-OrderService/src/main/java/com/example/ecommerce/order/rest/OrderOperationController.java
@@ -1,13 +1,19 @@
 package com.example.ecommerce.order.rest;
 
-import com.example.ecommerce.order.enity.Order;
-import com.example.ecommerce.order.service.IOrderServiceMgmt;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.example.ecommerce.order.enity.Order;
+import com.example.ecommerce.order.service.IOrderServiceMgmt;
 
 @RestController
 @RequestMapping("/order-api")
@@ -17,20 +23,20 @@ public class OrderOperationController {
     @PostMapping("/create-order")
     public ResponseEntity<Order> createOrder(@RequestBody Order order) {
         Order createdOrder = orderService.createOrder(order);
-        return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+        return new ResponseEntity<Order>(createdOrder, HttpStatus.CREATED);
     }
 
     @GetMapping("/show-order/{id}")
     public ResponseEntity<Order> showOrder(@PathVariable Long id) {
         Order showOrder = orderService.findOrderById(id);
-        return new ResponseEntity<>(showOrder, HttpStatus.OK);
+        return new ResponseEntity<Order>(showOrder, HttpStatus.OK);
     }
 
     @GetMapping("/show-order/all")
     public ResponseEntity<List<Order>> showOrdersList()
     {
         List<Order> showOrders = orderService.findAllOrders();
-        return new ResponseEntity<>(showOrders, HttpStatus.OK);
+        return new ResponseEntity<List<Order>>(showOrders, HttpStatus.OK);
     }
 
 }
